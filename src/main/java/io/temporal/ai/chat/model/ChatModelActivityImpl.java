a36d7ee846a0765798c7e80f6fc678ca0234c3a0
Quinn Klassen <klassenq@gmail.com>
2025-07-18 14:52:45 -0700

    Make NonTransientAiException fail activity

--- src/main/java/io/temporal/ai/chat/model/ChatModelActivityImpl.java
+++ src/main/java/io/temporal/ai/chat/model/ChatModelActivityImpl.java
@@ -1,14 +1,10 @@
 package io.temporal.ai.chat.model;
 
-import org.springframework.ai.chat.client.ChatClient;
+import io.temporal.failure.ApplicationErrorCategory;
+import io.temporal.failure.ApplicationFailure;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.stereotype.Component;
+import org.springframework.ai.retry.NonTransientAiException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,33 +19,42 @@ public class ChatModelActivityImpl implements ChatModelActivity {
 
     @Override
     public ChatModelTypes.ChatModelActivityOutput callChatModel(ChatModelTypes.ChatModelActivityInput input) {
-        ChatResponse response = chatModel.call(ActivityChatModel.createPrompt(input));
-        List<ChatModelTypes.ChatModelActivityOutput.Generation> generations = response.getResults().stream()
-                .map(generation -> new ChatModelTypes.ChatModelActivityOutput.Generation(
-                        ActivityChatModel.fromAssistantMessage(generation.getOutput())
+        try {
+            ChatResponse response = chatModel.call(ActivityChatModel.createPrompt(input));
+            List<ChatModelTypes.ChatModelActivityOutput.Generation> generations = response.getResults().stream()
+                    .map(generation -> new ChatModelTypes.ChatModelActivityOutput.Generation(
+                            ActivityChatModel.fromAssistantMessage(generation.getOutput())
 
-                ))
-                .collect(Collectors.toList());
+                    ))
+                    .collect(Collectors.toList());
 
-        ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata metadata = new ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata(
-                response.getMetadata().getModel(),
-                new ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata.RateLimit(
-                        response.getMetadata().getRateLimit().getRequestsLimit(),
-                        response.getMetadata().getRateLimit().getRequestsRemaining(),
-                        response.getMetadata().getRateLimit().getRequestsReset(),
-                        response.getMetadata().getRateLimit().getTokensLimit(),
-                        response.getMetadata().getRateLimit().getTokensRemaining(),
-                        response.getMetadata().getRateLimit().getTokensReset()
-                        ),
-                new ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata.Usage(
-                        response.getMetadata().getUsage().getPromptTokens(),
-                        response.getMetadata().getUsage().getCompletionTokens(),
-                        response.getMetadata().getUsage().getTotalTokens()
-                )
-        );
-        return new ChatModelTypes.ChatModelActivityOutput(
-                generations,
-                metadata
-        );
+            ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata metadata = new ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata(
+                    response.getMetadata().getModel(),
+                    new ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata.RateLimit(
+                            response.getMetadata().getRateLimit().getRequestsLimit(),
+                            response.getMetadata().getRateLimit().getRequestsRemaining(),
+                            response.getMetadata().getRateLimit().getRequestsReset(),
+                            response.getMetadata().getRateLimit().getTokensLimit(),
+                            response.getMetadata().getRateLimit().getTokensRemaining(),
+                            response.getMetadata().getRateLimit().getTokensReset()
+                    ),
+                    new ChatModelTypes.ChatModelActivityOutput.ChatResponseMetadata.Usage(
+                            response.getMetadata().getUsage().getPromptTokens(),
+                            response.getMetadata().getUsage().getCompletionTokens(),
+                            response.getMetadata().getUsage().getTotalTokens()
+                    )
+            );
+            return new ChatModelTypes.ChatModelActivityOutput(
+                    generations,
+                    metadata
+            );
+        } catch (NonTransientAiException e) {
+            throw ApplicationFailure.newBuilder()
+                    .setType(e.getClass().getTypeName())
+                    .setMessage(e.getMessage())
+                    .setCause(e.getCause())
+                    .setCategory(ApplicationErrorCategory.UNSPECIFIED)
+                    .build();
+        }
     }
 }
